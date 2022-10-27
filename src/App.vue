1d8d94997abf28d424141ac3f9229b9fcda368a4
zzc <875690678@qq.com>
2022-10-27 14:17:59 +0800

    feat: project init

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,99 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 // import HelloWorld from './components/HelloWorld.vue'
-import { DynamicIslandPlayer } from 'js-dynamic-island-player'
+import { DynamicIslandPlayer } from 'vue-dynamic-island-player'
+import { ref } from 'vue'
+import song1 from '@/assets/audio/rave_digger.mp3'
+import song2 from '@/assets/audio/80s_vibe.mp3'
+import cover1 from '@/assets/cover1.png'
+import cover2 from '@/assets/cover2.png'
+const player = ref()
+const playList = ref([
+  {
+    title: 'Rave Digger',
+    file: song1,
+    howl: null,
+    author: 'Cherrystones',
+    cover: cover1
+  },
+  {
+    title: '80s Vibe',
+    file: song2,
+    howl: null,
+    author: 'Tory Lanez',
+    cover: cover2
+  }
+])
+function test () {
+  console.log('233223')
+}
+function play () {
+  console.log('play')
+}
+function next () {
+  console.log('next')
+}
+function previous () {
+  console.log('previous')
+}
+function pause () {
+  console.log('pause')
+}
+function set () {
+  player.value.setVolume(0.9)
+}
+function setMute () {
+  player.value.setMute(true)
+}
+function setSound () {
+  player.value.setMute(false)
+}
+function seekTo () {
+  player.value.seekBySeconds(30)
+}
+function toggle () {
+  player.value.toggle()
+}
+function playNext () {
+  player.value.playNext()
+}
+function playPrevious () {
+  player.value.playPrevious()
+}
+function getState () {
+  console.log(player.value.getSoundState())
+}
 
 </script>
 
 <template>
   <div>
     <div id="iphone14pro">
-      <div style="margin-top: 25px">
-        <DynamicIslandPlayer/>
+      <div style="margin: 25px auto;">
+        <DynamicIslandPlayer
+            ref="player"
+            :play-list="playList"
+            :volume="0.8"
+            :html5="true"
+            @play="play"
+            @next="next"
+            @pause="pause"
+            @previous="previous"
+            @animation-small="test" />
+
       </div>
     </div>
+    <div class="control-area">
+      <button @click="getState">get sound state</button>
+      <button @click="set">set volume</button>
+      <button @click="setMute">set mute</button>
+      <button @click="setSound">set sound</button>
+      <button @click="seekTo">jumpTo 30s</button>
+      <button @click="toggle">play / pause</button>
+      <button @click="playNext">next song</button>
+      <button @click="playPrevious">previous song</button>
+    </div>
+
   </div>
 </template>
 <style scoped>
@@ -64,5 +146,11 @@ import { DynamicIslandPlayer } from 'js-dynamic-island-player'
   justify-content: center;
   background-position-x: 0,17px;
   background-repeat: no-repeat;
+}
+.control-area {
+  margin-top: 25px;
+}
+.control-area button {
+  margin: 3px 16px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import "../node_modules/js-dynamic-island-player/style.css"; //引入组件样式
+import "../node_modules/vue-dynamic-island-player/style.css"; //引入组件样式
 
 createApp(App).mount('#app')
